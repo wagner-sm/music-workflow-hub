b79c999cd5ca40750eec48f430eddac413e9c441
Wagner Moreira <wsmmetal@outlook.com>
2026-02-13 06:57:58 -0300

    Atualizacao

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -7,7 +7,7 @@ const Index = () => {
   
   const executeDiscogs = async () => {
     try {
-      const response = await fetch("https://workers.wagnermetalcfc.workers.dev/discogs", {
+      const response = await fetch("https://n8n-n8n.cdkzai.easypanel.host/webhook/discogs", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
@@ -38,7 +38,7 @@ const Index = () => {
 
   const executeSetlistFM = async () => {
     try {
-      const response = await fetch("https://workers.wagnermetalcfc.workers.dev/setlistfm", {
+      const response = await fetch("https://n8n-n8n.cdkzai.easypanel.host/webhook/setlistfm", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
